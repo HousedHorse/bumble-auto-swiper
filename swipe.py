ee10a5d2d8b5d6f3747d5ce4534b60ae9ff30d30
HousedHorse <william@wfindlay.com>
2019-04-10 23:23:05 -0400

    all done

---
+++ swipe.py
@@ -0,0 +1,84 @@
+#! /usr/bin/python
+from selenium import webdriver
+from selenium.common.exceptions import *
+
+import argparse
+
+def log_in_with_facebook(driver, email, password):
+    while True:
+        try:
+            driver.find_element_by_xpath("//div[contains(@class, 'button--provider-facebook')]").click()
+            break
+        except NoSuchElementException:
+            pass
+
+    # switch to facebook window
+    driver.switch_to.window(driver.window_handles[1])
+
+    # try to enter email until it loads
+    while True:
+        try:
+            driver.find_element_by_xpath("//input[@id='email']").send_keys(email)
+            break
+        except NoSuchElementException as e:
+            pass
+
+    # enter password and click submit
+    driver.find_element_by_xpath("//input[@id='pass']").send_keys(password)
+    driver.find_element_by_xpath("//input[@id='u_0_0']").click()
+
+    try:
+        # try to click continue until it loads
+        while True:
+            try:
+                driver.find_element_by_xpath("//button[contains(text(), 'Continue as')]").click()
+                break
+            except NoSuchElementException:
+                pass
+    except:
+        pass
+
+    # back to main window
+    driver.switch_to.window(driver.window_handles[0])
+
+def swipe_right(driver):
+    while True:
+        # swipe right if the window is loaded
+        try:
+            driver.find_element_by_xpath("//div[contains(@class, 'encounters-action--like')]").click()
+            return True
+        except NoSuchElementException:
+            pass
+
+        # check to see if we are all caught up
+        try:
+            driver.find_element_by_xpath("//span[contains(text(), 'all caught up')]")
+            return False
+        except NoSuchElementException:
+            pass
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Auto swiper for bumble. Pass your Facebook email and password.")
+    parser.add_argument('email', metavar='email', type=str, nargs=1,
+            help='Your Facebook email')
+    parser.add_argument('password', metavar='password', type=str, nargs=1,
+            help='Your Facebook password')
+
+    args     = parser.parse_args()
+    email    = args.email
+    password = args.password
+
+    # open driver
+    driver = webdriver.Firefox()
+    driver.get("https://bumble.com/login")
+
+    log_in_with_facebook(driver, email, password)
+
+    # keep swiping right until we're caught up
+    while True:
+        if not swipe_right(driver):
+            print("All caught up!")
+            break
+
+    driver.close()
